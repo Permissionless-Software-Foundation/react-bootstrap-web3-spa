7c366bf39930388d1c18f1fb254b3094aba658ed
Daniel Gonzalez <danielhumgon@gmail.com>
2025-01-17 13:55:50 -0400

    fix(update): Updated to use latest react and react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 /*
-*/
+ */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryParamProvider } from 'use-query-params'
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
-  <>
-    <QueryParamProvider>
-      <App />
-    </QueryParamProvider>
-  </>
-  , document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <QueryParamProvider>
+    <App />
+  </QueryParamProvider>
+)
 
 // Updating to React v18
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
